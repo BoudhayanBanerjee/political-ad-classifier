cdb70b1fa7f616e2dcea83abd6fd27eafb9eacf4
Boudhayan Banerjee <boudhayanbanerjee@gmail.com>
2017-06-19 07:47:42 -0500

    starting

---
+++ adclassifier/feature_extraction.py
@@ -0,0 +1,6 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# TODO
+# compare vectorizers
---
+++ adclassifier/feature_selection.py
@@ -0,0 +1,7 @@
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import RFE
+
+# TODO
